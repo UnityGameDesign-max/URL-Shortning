0710b535f9189099a4f593ec744e54e0b0e16146
Tumelo Mahlangu <tumie201511@gmail.com>
2023-09-23 14:48:02 +0200

    added Boost component

---
+++ src/widgets/Boost.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Boost(){
+    return(
+        <div className='boost-pattern'>
+            <div className="container align-items-row">
+                <h1 className='title'>Boost your links today</h1>
+                <button className='btn'>Get Started</button>
+            </div>
+        </div>
+    )
+}
